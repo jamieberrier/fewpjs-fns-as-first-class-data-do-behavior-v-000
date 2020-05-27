720a06229ed5223b53a3acbd8acfe0e4dd5e87d0
Jamie Berrier <berrier.jamie@gmail.com>
2020-05-27 21:53:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function greet(timeString) {
   const hour = parseInt(time[0], 10);
   if (hour < 12) {
     displayMessage('Good Morning');
-  } else if (hour.between?(12, 17)) {
+  } else if (hour.between?(12, 17) {
     displayMessage('Good Afternoon');
   } else {
     displayMessage('Good Evening');
