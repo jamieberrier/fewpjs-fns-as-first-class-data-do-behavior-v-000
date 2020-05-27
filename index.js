eb68f171c160fb008933fc44eacac179dc35e6e1
Jamie Berrier <berrier.jamie@gmail.com>
2020-05-27 22:06:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,5 +27,5 @@ function greet(timeString) {
 // It does not return anything.
 // <h1 id="greeting"></h1>
 function displayMessage(message) {
-  return document.getElementById('greeting').innerText = message;
+  document.getElementById('greeting').innerText = message;
 }
